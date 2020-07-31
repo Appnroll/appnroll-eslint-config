8acbb964446ab3f1d74710089aab25595f193cc3
Mateusz Kocz <mateusz@kocz.pl>
2020-07-31 10:38:19 +0200

    Add some configurations and change the package name

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,36 +3,43 @@ module.exports = {
     "browser": true
   },
   extends: [
+    "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
     "prettier/@typescript-eslint",
     "plugin:jsx-a11y/recommended",
     "plugin:jest-dom/recommended",
-    "plugin:testing-library/recommended"
+    "plugin:testing-library/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     "project": "tsconfig.json",
     "sourceType": "module"
   },
-  plugins: ["@typescript-eslint", "jsx-a11y", "testing-library", "jest-dom"],
+  plugins: [
+    "@typescript-eslint",
+    "jsx-a11y",
+    "testing-library",
+    "jest-dom",
+    "react",
+    "react-hooks"
+  ],
   rules: {
-    "interface-name": 0,
-    "no-console": 0,
-    "object-literal-sort-keys": 0,
-    "jsx-boolean-value": 0,
-    "@typescript-eslint/no-var-requires": 0,
-    "@typescript-eslint/ban-types": 1,
-    "@typescript-eslint/no-empty-interface": 1,
-    "@typescript-eslint/restrict-plus-operands": 1,
-    "@typescript-eslint/ban-ts-comment": 0,
-    "react-hooks/rules-of-hooks": 1,
-    "@typescript-eslint/no-unsafe-assignment": 1,
-    "@typescript-eslint/no-unsafe-member-access": 1,
-    "@typescript-eslint/no-unsafe-call": 1,
-    "@typescript-eslint/restrict-template-expressions": 1,
-    "@typescript-eslint/no-floating-promises": 1,
-    "@typescript-eslint/no-unsafe-return": 1
+    "jsx-boolean-value": ["warn", "always"],
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/ban-types": "warn",
+    "@typescript-eslint/no-empty-interface": "warn",
+    "@typescript-eslint/restrict-plus-operands": "warn",
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "react-hooks/rules-of-hooks": "warn",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/restrict-template-expressions": "warn",
+    "@typescript-eslint/no-floating-promises": "warn",
+    "@typescript-eslint/no-unsafe-return": "warn"
   }
 }
